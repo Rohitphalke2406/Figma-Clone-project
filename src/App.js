0481c8308339a447024a8053857819f47aa0a4f7
Rohit Phalke <rohitphalke165@gmail.com>
2024-04-09 01:26:21 +0530

    done with Header and Footer

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from './components/HeaderFooter/Header';
+import Footer from './components/HeaderFooter/Footer';
+
+function App() {
+  return (
+    <div>
+       <Header/>
+       <Footer/>
+    </div>
+  );
+}
+
+export default App;
